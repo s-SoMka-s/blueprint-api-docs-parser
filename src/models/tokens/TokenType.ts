a631f9bc6ba80afe1c6c11eb5c8790e2363acc61
i.chiesov <i.chiesov@gmail.com>
2022-04-30 17:26:44 +0700

    Add tokens

--- src/models/tokens/TokenType.ts
+++ src/models/tokens/TokenType.ts
@@ -1,3 +1,18 @@
-export enum TokenType {
-    
+export default class TokenType {
+    name: string;
+    regex: string;
+
+
+    constructor(name: string, regex: string) {
+        this.name = name;
+        this.regex = regex;
+    }
+}
+
+export const tokenTypesList = {
+    'HOST': new TokenType('HOST', "HOST:"),
+    'COLON': new TokenType('COLON', ":"),
+    'URL': new TokenType('URL', "https:\/\/[0-9.a-zA-Z:]*\/"),
+    'SEMICOLON': new TokenType('SEMICOLON', ';'),
+    'SPACE': new TokenType('SPACE', '[ \\n\\t\\r]')
 }
