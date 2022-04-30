d6671066ae0505a9686aa4d69cc815e8b8aea692
i.chiesov <i.chiesov@gmail.com>
2022-04-30 17:27:04 +0700

    Simple lexer

---
+++ src/Lexer.ts
@@ -0,0 +1,49 @@
+import Token from "./models/tokens/Token";
+import { tokenTypesList } from "./models/tokens/TokenType";
+
+export default class Lexer {
+    code: string;
+    pos: number = 0;
+    tokenList: Token[] = []
+
+    constructor(code: string) {
+        this.code = code;
+    }
+
+    lexAnalisys(): Token[] {
+        const tokens: Token[] = []
+        while (this.hasNextToken()) {
+            const token = this.getNextToken();
+            if (token) {
+                tokens.push(token);
+            }
+        }
+
+        return tokens;
+    }
+
+    private hasNextToken(): boolean {
+        if (this.pos >= this.code.length) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private getNextToken(): Token | null {
+        const tokenTypesValues = Object.values(tokenTypesList)
+        
+        for (let i = 0; i < tokenTypesValues.length; i++) {
+            const tokenType = tokenTypesValues[i];
+            const regex = new RegExp('^' + tokenType.regex);
+            const result = this.code.substring(this.pos).match(regex)
+            if (result && result[0]) {
+                const token = new Token(tokenType, result[0], this.pos);
+                this.pos += result[0].length;
+                return token;
+            }
+        }
+
+        return null;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-const str: string = "ТЕСТ";
+import fs from "fs";
+import Lexer from "./Lexer";
 
-console.log(str);
+const buffer = fs.readFileSync("assets/examples/simple.api.apib");
+const fileContent = buffer.toString();
+
+const lexer = new Lexer(fileContent);
+const tokens = lexer.lexAnalisys()
+console.log(tokens);
