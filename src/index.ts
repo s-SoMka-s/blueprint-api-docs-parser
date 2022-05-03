fc6a879a4069ddc6738593bef4fdb72c95c69916
i.chiesov <i.chiesov@gmail.com>
2022-05-03 09:14:24 +0700

    Add metadata section parsing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import Lexer from "./Lexer";
 
-const buffer = fs.readFileSync("assets/examples/simple.api.apib");
+const buffer = fs.readFileSync("assets/examples/metadata.example.apib");
 const fileContent = buffer.toString();
 
 const lexer = new Lexer(fileContent);
--- src/models/tokens/TokenType.ts
+++ src/models/tokens/TokenType.ts
@@ -1,3 +1,5 @@
+import Token from "./Token";
+
 export default class TokenType {
     name: string;
     regex: string;
@@ -10,9 +12,21 @@ export default class TokenType {
 }
 
 export const tokenTypesList = {
-    'HOST': new TokenType('HOST', "HOST:"),
-    'COLON': new TokenType('COLON', ":"),
+    // metadata section
+    'FORMAT': new TokenType('FORMAT', 'FORMAT'),
+    'HOST': new TokenType('HOST', "HOST"),
+
+    'FIRST_MARKDOWN_HEADER': new TokenType('FIRST_MARKDOWN_HEADER', '#'),
+    'SECOND_MARKDOWN_HEADER': new TokenType('SECOND_MARKDOWN_HEADER', '##'),
+    'THIRD_MARKDOWN_HEADER': new TokenType('THIRD_MARKDOWN_HEADER', '###'),
+
     'URL': new TokenType('URL', "https:\/\/[0-9.a-zA-Z:]*\/"),
+
+    'WORD': new TokenType('WORD', '[0-9A-Za-z]*'),
+
+    'COLON': new TokenType('COLON', ":"),
+    'SPACE': new TokenType('SPACE', ' '),
+    
     'SEMICOLON': new TokenType('SEMICOLON', ';'),
-    'SPACE': new TokenType('SPACE', '[ \\n\\t\\r]')
+    'LINE_BREAK': new TokenType('LINE_BREAK', '\\r\\n'),
 }
