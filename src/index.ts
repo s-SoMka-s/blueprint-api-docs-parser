83699a9c63699511e957c615b867a2529279272d
i.chiesov <i.chiesov@gmail.com>
2022-05-03 09:19:03 +0700

    Parse api name overview section

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import fs from "fs";
 import Lexer from "./Lexer";
 
-const buffer = fs.readFileSync("assets/examples/metadata.example.apib");
+const metadata = "assets/examples/metadata.example.apib";
+const apiNameOverview = "assets/examples/api-name-overview.example.apib";
+
+const buffer = fs.readFileSync(apiNameOverview);
 const fileContent = buffer.toString();
 
 const lexer = new Lexer(fileContent);
