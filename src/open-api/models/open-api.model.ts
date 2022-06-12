20fbad77d9eafef4d5eb4c8cd0d63a2b43fb6faa
s-SoMka-s <i.chiesov@gmail.com>
2022-06-12 20:17:40 +0700

    Add base builder.

--- src/open-api/models/open-api.model.ts
+++ src/open-api/models/open-api.model.ts
@@ -1,14 +1,14 @@
-import { IInfo } from "./info/info.model";
-import { IPathItem } from "./path/path-item.model";
-import { ISecurity } from "./security.model";
-import { IServer } from "./server.model";
-import { ITag } from "./tag.model";
+import { IInfo } from './info/info.model';
+import { IPathItem } from './path/path-item.model';
+import { ISecurityRequirement } from './security-requirement.model';
+import { IServer } from './server.model';
+import { ITag } from './tag.model';
 
 export interface IOpenApi {
     openapi: string;
-    info: IInfo;
+    info: IInfo | null;
     servers: IServer[];
     paths: Map<string, IPathItem>;
-    security: ISecurity[];
+    security: ISecurityRequirement[];
     tags: ITag[];
 }
